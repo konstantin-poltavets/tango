34c4e4b0d47053e6dbe17cd1f9a6fc4a9548fa91
konstantin.poltaets <konstantin.poltaets@gmail.com>
2018-03-03 15:13:53 +0200

    '2'

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -26,4 +26,6 @@ url(r'^admin/', admin.site.urls),
 url(r'^about/$', views.about, name='about'),
 url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
 url(r'^add_category/$', views.add_category, name='add_category'),
+url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
